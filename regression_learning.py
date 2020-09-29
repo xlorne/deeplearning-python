5dfe589c9f1fb714792345a4431696152ccff895
1991wangliang <1991wangliang@gmail.com>
2020-09-29 10:46:15 +0800

    Initial commit

---
+++ regression_learning.py
@@ -0,0 +1,58 @@
+from IPython import display
+from matplotlib import pyplot as plt
+from mxnet import autograd, nd
+import random
+
+
+
+num_inputs = 2
+num_examples = 1000
+true_w = [2, -3.4]
+true_b = 4.2
+features = nd.random.normal(scale=1, shape=(num_examples, num_inputs))
+labels = true_w[0] * features[:, 0] + true_w[1] * features[:, 1] + true_b
+labels += nd.random.normal(scale=0.01, shape=labels.shape)
+
+def use_svg_display():
+    # 用矢量图显示
+    display.set_matplotlib_formats('svg')
+
+def set_figsize(figsize=(3.5, 2.5)):
+    use_svg_display()
+    # 设置图的尺寸
+    plt.rcParams['figure.figsize'] = figsize
+
+set_figsize()
+plt.scatter(features[:, 1].asnumpy(), labels.asnumpy(), 1);  # 加分号只显示图
+
+
+
+# 本函数已保存在d2lzh包中方便以后使用
+def data_iter(batch_size, features, labels):
+    num_examples = len(features)
+    indices = list(range(num_examples))
+    random.shuffle(indices)  # 样本的读取顺序是随机的
+    for i in range(0, num_examples, batch_size):
+        j = nd.array(indices[i: min(i + batch_size, num_examples)])
+        yield features.take(j), labels.take(j)  # take函数根据索引返回对应元素
+
+
+batch_size = 10
+
+for X, y in data_iter(batch_size, features, labels):
+    print(X, y)
+    break
+
+
+w = nd.random.normal(scale=0.01, shape=(num_inputs, 1))
+b = nd.zeros(shape=(1,))
+
+w.attach_grad()
+b.attach_grad()
+
+
+def linreg(X, w, b):  # 本函数已保存在d2lzh包中方便以后使用
+    return nd.dot(X, w) + b
+
+def squared_loss(y_hat, y):  # 本函数已保存在d2lzh包中方便以后使用
+    return (y_hat - y.reshape(y_hat.shape)) ** 2 / 2
