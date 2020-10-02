14580c1fbc34600e7712ce938ae91a6c7acc1a2c
wangliang <1991wangliang@gmail.com>
2020-10-02 18:05:16 +0800

    导数计算 与 简单线性回归方程

---
+++ autograd_test.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File Name: autograd_test.py
+# Description:
+# Create Time: 2020-10-02 17:18
+# Author: lorne
+
+
+from mxnet import autograd, nd
+
+
+w = nd.random.normal(scale=0.01, shape=(2, 1))
+b = nd.random.normal(scale=0.05,shape=(1,1))
+
+print(w)
+print(b)
+
+b.attach_grad()
+w.attach_grad()
+
+def fx(b,w):
+    return 3*b+4*w
+
+
+with autograd.record():
+    z=fx(b,w)
+z.backward()
+
+print(b.grad)
+print(w.grad)
+
+
+# b\prime = 6 因为bshape为1，1 在做运算时加了两次固是2x3=6
--- regression_learning.py
+++ regression_learning.py
@@ -13,19 +13,18 @@ true_w = [2, -3.4]
 true_b = 4.2
 features = nd.random.normal(scale=1, shape=(num_examples, num_inputs))
 labels = true_w[0] * features[:, 0] + true_w[1] * features[:, 1] + true_b
-labels += nd.random.normal(scale=0.01, shape=labels.shape)
+# 增加干扰数据
+labels += nd.random.normal(scale=0.1, shape=labels.shape)
 
-def use_svg_display():
-    # 用矢量图显示
+# 图形化展示界面
+def initPlt(figsize=(4.5, 4.5)):
     display.set_matplotlib_formats('svg')
-
-def set_figsize(figsize=(3.5, 2.5)):
-    use_svg_display()
     # 设置图的尺寸
     plt.rcParams['figure.figsize'] = figsize
 
-set_figsize()
-plt.scatter(features[:, 1].asnumpy(), labels.asnumpy(), 1);  # 加分号只显示图
+initPlt()
+plt.scatter(features[:, 1].asnumpy(), labels.asnumpy(), 1)  # 加分号只显示图
+plt.show()
 
 
 
@@ -58,3 +57,24 @@ def linreg(X, w, b):  # 本函数已保存在d2lzh包中方便以后使用
 
 def squared_loss(y_hat, y):  # 本函数已保存在d2lzh包中方便以后使用
     return (y_hat - y.reshape(y_hat.shape)) ** 2 / 2
+
+def sgd(params, lr, batch_size):  # 本函数已保存在d2lzh包中方便以后使用
+    for param in params:
+        param[:] = param - lr * param.grad / batch_size
+
+
+lr = 0.03
+num_epochs = 3
+net = linreg
+loss = squared_loss
+
+for epoch in range(num_epochs):  # 训练模型一共需要num_epochs个迭代周期
+    # 在每一个迭代周期中，会使用训练数据集中所有样本一次（假设样本数能够被批量大小整除）。X
+    # 和y分别是小批量样本的特征和标签
+    for X, y in data_iter(batch_size, features, labels):
+        with autograd.record():
+            l = loss(net(X, w, b), y)  # l是有关小批量X和y的损失
+        l.backward()  # 小批量的损失对模型参数求梯度
+        sgd([w, b], lr, batch_size)  # 使用小批量随机梯度下降迭代模型参数
+    train_l = loss(net(features, w, b), labels)
+    print('epoch %d, loss %f' % (epoch + 1, train_l.mean().asnumpy()))
