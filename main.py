460fba12553a6f0855fadfbe934e1fd3324cf230
wangliang <1991wangliang@gmail.com>
2020-12-03 16:29:28 +0800

    server test

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+import mxnet as mx
+from mxnet import nd
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -12,5 +13,7 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    data = [nd.ones((1, 2), ctx=mx.gpu(i)) * (i + 1) for i in range(2)]
+    print(data)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
